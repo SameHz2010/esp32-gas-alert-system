d3b0a4aa25881608e8aca4745d25512502329623
DESKTOP-VUB1IRJ\Thinkbook <nguyenhuuphong53@gmail.com>
2026-08-04 14:28:32 +0700

    feat: add wifi event handler node 3

--- BackEnd/GasSensor_node3/include/services.h
+++ BackEnd/GasSensor_node3/include/services.h
@@ -1,6 +1,23 @@
 #pragma once
 
+#include <time.h>
+#include <Arduino.h>
+
+// Quản lý kết nối WiFi & Đếm sự kiện ngắt kết nối
 bool initWiFi();
+bool isWiFiConnected();
+uint32_t getWiFiDisconnectEventCount();
+uint32_t getWiFiGotIpEventCount();
+
+// Khởi tạo Thời gian (NTP) & Firebase Realtime Database
 bool initTime();
 void initFirebase();
-void uploadData(float temperature, float humidity, int gas, float deltaGas, float gasRelative, int state, const tm &info);
+
+// Đẩy dữ liệu cảm biến Node 2 lên Firebase
+bool uploadData(float temperature,
+                float humidity,
+                int gas,
+                float deltaGas,
+                float gasRelative,
+                int state,
+                const tm &info);
